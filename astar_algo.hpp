ac78cba4a8d4770e1f0f0873aeeb02ec48994f52
Flaviu <cflaviu@users.noreply.github.com>
2023-10-02 09:49:21 +0300

    update

--- astar_algo.hpp
+++ astar_algo.hpp
@@ -1,8 +1,9 @@
 ﻿/// A* Incremental Search Algorithm
 /// Copyright (c) Flaviu Cibu. All rights reserved.
-/// Created 06-sep-‎2009
-/// Updated 26-nov-‎2010
-/// Updated 24-apr-‎2022
+/// Created 06-sep-2009
+/// Updated 26-nov-2010
+/// Updated 24-apr-2022
+/// Updated 25-sep-2023
 #pragma once
 #ifndef PCH
     #include <utility>
@@ -15,9 +16,9 @@ namespace stdext::astar
     /// heuristic score (h). For more details see
     /// http://en.wikipedia.org/wiki/A*_search_algorithm#Algorithm_description. The
     /// descendant of this class has to provide two methods: the distance/cost to
-    /// another node (signature Value distance_to( const note_type& node) const) and
+    /// another node (signature Value distance_to( const node_type& node) const) and
     /// the estimated (heuristic) distance/cost to target node (signature void
-    /// set_heuristic_score( const note_type& target_node)).
+    /// set_heuristic_score( const node_type& target_node)).
     template <typename _Score>
     class base_node
     {
@@ -42,8 +43,14 @@ namespace stdext::astar
         /// Sets the heuristic score (h).
         void set_heuristic_score(const score_type value) noexcept { heuristic_score_ = value; }
 
-        /// Operator < - used on sorting of the priority queue of open set.
-        bool operator<(const base_node& node) const noexcept { return total_score() < node.total_score(); }
+        void clear() noexcept
+        {
+            general_score_ = {};
+            heuristic_score_ = {};
+        }
+
+        /// Operator > - used on sorting of the priority queue of open set.
+        bool operator>(const base_node& node) const noexcept { return total_score() > node.total_score(); }
 
     protected:
         score_type general_score_ {};
@@ -53,7 +60,7 @@ namespace stdext::astar
     /// Dummy beam search functor.
     struct no_beam_search
     {
-        void operator()(auto&, auto&, auto&) const noexcept {}
+        bool operator()(auto&, auto&, auto&, auto&) const noexcept { return false; }
     };
 
     /// @brief Generic C++ implementation of A* algorithm
@@ -61,47 +68,57 @@ namespace stdext::astar
     /// customizable internal data structures, step-by-step execution and beam
     /// search support.
     template <typename _Node, typename _PriorityQueue, typename _NeighborEnumerator, typename _Set, typename _SolutionVerifier,
-              typename _Solution, typename _BeamSearch = no_beam_search>
+              typename _SolutionMap, typename _BeamSearch = no_beam_search>
     class algo
     {
     public:
-        using note_type = _Node;
+        using node_type = _Node;
         using priority_queue_type = _PriorityQueue;
         using set_type = _Set;
         using neighbor_enumerator_type = _NeighborEnumerator;
         using solution_verifier_type = _SolutionVerifier;
-        using solution_type = _Solution;
+        using solution_map_type = _SolutionMap;
         using beam_search_type = _BeamSearch;
 
         /// @param[in] start_node Start node
         /// @param[in] target_node Target node
-        /// @param[in] solution_verifier solution_type verifier functor - checks if
+        /// @param[in] solution_verifier solution_map_type verifier functor - checks if
         /// the current node is the target node.
         /// @param[in] neighbor_enumerator Enumerator of adjacent nodes
         /// @param[in] beam_search Beach search - filter of the items from open set.
         /// See more details at
         /// http://theory.stanford.edu/~amitp/GameProgramming/Variations.html#S1. By
         /// default the beam search is disable using a dummy filter no_beam_search.
-        algo(note_type start_node, note_type target_node, solution_verifier_type solution_verifier,
+        algo(node_type start_node, node_type target_node, solution_verifier_type solution_verifier,
              neighbor_enumerator_type neighbor_enumerator, beam_search_type beam_search):
-            _solution_verifier(std::move(solution_verifier)),
-            _beam_search(std::move(beam_search)),
-            _neighbor_enumerator(std::move(neighbor_enumerator)),
-            _target_node(std::move(target_node))
+            solution_verifier_(std::move(solution_verifier)),
+            beam_search_(std::move(beam_search)),
+            neighbor_enumerator_(std::move(neighbor_enumerator)),
+            target_node_(std::move(target_node))
         {
-            _priority_open_set.emplace(std::move(start_node));
-            _open_set.emplace(std::move(start_node));
+            start_node.set_heuristic_score(0, target_node_);
+            open_set_.insert(start_node);
+            priority_open_set_.push(std::move(start_node));
         }
 
         /// Checks if the solution was found. If the return is true, the solution can
         /// be used by calling @ref solution method.
-        bool has_solution() const noexcept { return _has_solution; }
+        bool has_solution() const noexcept { return has_solution_; }
 
         /// Gets the solution instance.
-        const solution_type& solution() const noexcept { return _solution; }
+        const solution_map_type& solution() const noexcept { return solution_; }
 
         /// Gets the solution instance.
-        solution_type& solution() noexcept { return _solution; }
+        solution_map_type& solution() noexcept { return solution_; }
+
+        /// Gets the solution verifier.
+        const solution_verifier_type& solution_verifier() const noexcept { return solution_verifier_; }
+
+        /// Gets the last used node.
+        const node_type& node() const noexcept { return node_; }
+
+        /// Gets the beam search object.
+        const beam_search_type& beam_search() const noexcept { return beam_search_; }
 
         /// @brief algo progress method - useful for fined grained execution, early
         /// exit (see
@@ -118,38 +135,14 @@ namespace stdext::astar
         bool operator()()
         {
             bool can_continue = false;
-            if (!_open_set.empty())
+            if (!open_set_.empty())
             {
-                _node = _priority_open_set.top();
-                _has_solution = _solution_verifier(_node);
-                if (!_has_solution)
+                node_ = priority_open_set_.top();
+                has_solution_ = solution_verifier_(node_);
+                if (!has_solution_)
                 {
                     can_continue = true;
-                    _priority_open_set.pop();
-                    _open_set.erase(_node);
-                    _closed_set.insert(_node);
-                    _neighbor_enumerator(_node);
-                    while (_neighbor_enumerator && _closed_set.find(*_neighbor_enumerator) == _closed_set.end())
-                    {
-                        note_type& neighbor = *_neighbor_enumerator;
-                        auto tentative_general_score = _node.general_score() + _node.distance_to(neighbor);
-                        const bool need_test = _open_set.find(neighbor) == _open_set.end();
-                        if (need_test || (tentative_general_score < neighbor.general_score()))
-                        {
-                            neighbor.set_general_score(tentative_general_score);
-                            neighbor.set_heuristic_score(_target_node);
-                            if (need_test)
-                            {
-                                _priority_open_set.push(neighbor);
-                                _open_set.insert(neighbor);
-                                _beam_search(neighbor, _priority_open_set, _open_set);
-                            }
-
-                            _solution.insert({neighbor, _node});
-                        }
-
-                        ++_neighbor_enumerator;
-                    }
+                    evaluate_neighbors();
                 }
             }
 
@@ -157,15 +150,40 @@ namespace stdext::astar
         }
 
     protected:
-        solution_verifier_type _solution_verifier;
-        beam_search_type _beam_search;
-        neighbor_enumerator_type _neighbor_enumerator;
-        priority_queue_type _priority_open_set;
-        set_type _open_set;
-        set_type _closed_set;
-        solution_type _solution;
-        note_type _node;
-        note_type _target_node;
-        bool _has_solution {};
+        void evaluate_neighbors()
+        {
+            priority_open_set_.pop();
+            open_set_.erase(node_);
+            closed_set_.insert(node_);
+            for (neighbor_enumerator_(node_); neighbor_enumerator_; ++neighbor_enumerator_)
+                if (closed_set_.find(*neighbor_enumerator_) == closed_set_.end())
+                {
+                    node_type& neighbor = *neighbor_enumerator_;
+                    const auto tentative_general_score = node_.general_score() + node_.distance_to(neighbor);
+                    const bool need_test = open_set_.find(neighbor) == open_set_.end();
+                    if (need_test || (tentative_general_score < neighbor.general_score()))
+                    {
+                        neighbor.set_general_score(tentative_general_score);
+                        neighbor.set_heuristic_score(tentative_general_score, target_node_);
+                        if (!beam_search_(neighbor, solution_, open_set_, priority_open_set_))
+                        {
+                            solution_[neighbor] = node_;
+                            open_set_.insert(neighbor);
+                            priority_open_set_.push(neighbor);
+                        }
+                    }
+                }
+        }
+
+        solution_verifier_type solution_verifier_;
+        beam_search_type beam_search_;
+        neighbor_enumerator_type neighbor_enumerator_;
+        priority_queue_type priority_open_set_;
+        set_type open_set_;
+        set_type closed_set_;
+        solution_map_type solution_;
+        node_type node_;
+        node_type target_node_;
+        bool has_solution_ {};
     };
 } // namespace stdext::astar
